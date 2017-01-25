abf058ffb9c3319110278636f7b56088bcb733e6
amit chaudhary <amit chaudhary@DONNS>
2017-01-25 13:23:41 +0530

    added Dto Api and now more stable

---
+++ TheVidStore/Dtos/MovieDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using TheVidStore.Models;
+
+namespace TheVidStore.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+        [Range(0, 20)]
+        public int NumberOfMovies { get; set; }
+        public DateTime? Year { get; set; }
+        public string GenreOfMovie { get; set; }
+    }
+}
